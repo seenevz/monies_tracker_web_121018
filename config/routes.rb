aa486e15245bd8307e387a180e2ee21ce0e5e81a
seenevz <serneves@hotmail.com>
2019-01-22 15:23:33 +0000

    added views files and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users 
+  resources :personal_expenses, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :shared_wallets, only: [:show, :create, :update]
+
 end
