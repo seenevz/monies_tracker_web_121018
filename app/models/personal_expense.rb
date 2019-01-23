9907b60cf8cce7b29e53d0a69e06e0d5d605315d
seenevz <serneves@hotmail.com>
2019-01-23 18:01:47 +0000

    added forms

--- app/models/personal_expense.rb
+++ app/models/personal_expense.rb
@@ -2,8 +2,17 @@ class PersonalExpense < ApplicationRecord
     belongs_to :user
     has_one :shared_wallet
     belongs_to :category
+    accepts_nested_attributes_for :category
 
     # validates :name, :amount, presence: true
+
+    def category_attribures=(category_attribute)
+        
+          category = Category.find_or_create_by(category_attribute)
+          byebug
+          self.categories << category
+        
+    end
 
     def category_name
         self.category.name
